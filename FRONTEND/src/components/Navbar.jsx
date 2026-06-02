977102b45bb1b7a6a465380f156cae73730c0d6d
Benjamin Baraza <barazabenjamin58@gmail.com>
2026-06-02 16:47:51 +0300

    shadcn

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -6,9 +6,7 @@ const Navbar = () => {
       <nav className="sticky top-0 backdrop-filter backdrop-blur-lg bg-white/30 border-b border-white/20 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"></div>
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <Navbar.Brand />
-          </div>
+          <div className="flex items-center"></div>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
               <a
